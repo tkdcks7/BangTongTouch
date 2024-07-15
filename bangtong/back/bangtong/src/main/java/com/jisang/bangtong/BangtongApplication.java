35ec3cbb583f0c306f484772a292fd6ef163c688
jiwon <idjjm95@gmail.com>
2024-07-15 13:15:08 +0900

    add: 2주차 spring 프로젝트 추가

---
+++ bangtong/back/bangtong/src/main/java/com/jisang/bangtong/BangtongApplication.java
@@ -0,0 +1,13 @@
+package com.jisang.bangtong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangtongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangtongApplication.class, args);
+	}
+
+}
