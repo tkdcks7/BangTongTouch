5fa59773fbe122d60118d34843a164e945abea2d
강상찬 <tkdcks7@gmail.com>
2024-07-17 09:59:51 +0900

    add: react-cookie 라이브러리 설치 및 router.ts 일부 수정

--- bangtong/front/src/router.tsx
+++ bangtong/front/src/router.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import BtnGreen from './components/atoms/BtnGreen';
 import Layout from './components/layout/Layout';
+import LoginPage from './components/page/LoginPage';
+import SignupPage from './components/page/SignupPage';
 
+
+// 로그인페이지와 회원가입페이지는 Nav가 없기 때문에 Layout 밖에 선언함
 const Router: React.FC = () => {
   return (
   <BrowserRouter>
     <Routes>
-    {/* <Route path="/" element={<BtnGreen />} /> */}
-    <Route path='/' element={<Layout />}>
-      로그인
-    </Route>
+      <Route path='/user/login' element={<LoginPage />} />
+      <Route path='/user/register' element={<SignupPage />} />
+    {/* <Route path='/' element={<Layout />}>
+    이 사이에 Nav가 포함돼있는 기타 페이지를 작성하면 됩니다.
+    </Route> */}
 
     </Routes>
   </BrowserRouter>
