bd9c1a8886fa242a05f01d503d2e38e146dc378a
민서령 <msr980929@gmail.com>
2024-07-18 13:02:39 +0900

    remove: Security 관련 기능 제거

--- bangtong/back/src/main/java/com/jisang/bangtong/controller/alarmsetting/AlarmSettingController.java
+++ bangtong/back/src/main/java/com/jisang/bangtong/controller/alarmsetting/AlarmSettingController.java
@@ -3,33 +3,33 @@ package com.jisang.bangtong.controller.alarmsetting;
 
 import com.jisang.bangtong.dto.alarmsetting.AlarmSettingDto;
 import com.jisang.bangtong.dto.common.ResponseDto;
-import com.jisang.bangtong.model.alarmSetting.AlarmSetting;
 import com.jisang.bangtong.service.alarmsetting.AlarmSettingService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/alarms")
-@RequiredArgsConstructor
 @Slf4j
 public class AlarmSettingController {
-    @Autowired
-    private AlarmSettingService alarmSettingService;
 
-    @GetMapping("/test")
-    public void gettest(){
-        log.info("asklml");
-    }
+  @Autowired
+  private AlarmSettingService alarmSettingService;
 
-    @PostMapping("/setting/modify/{userId}")
-    public ResponseEntity<ResponseDto<Void>> updatealarmsetting(@PathVariable("userId") Long userId, @RequestBody AlarmSettingDto settingDto) {
-        userId = 1L;
-        System.out.println("업데이트");
-        alarmSettingService.updateAlarmSetting(userId, settingDto);
-        return ResponseEntity.ok(ResponseDto.res("SUCCESS"));
-    }
+  @GetMapping("/test")
+  public void gettest() {
+    log.info("asklml");
+  }
+
+  @PutMapping("/setting/modify/{userId}")
+  public ResponseDto<Void> updatealarmsetting(@PathVariable("userId") Long userId,
+      @RequestBody AlarmSettingDto settingDto) {
+    alarmSettingService.updateAlarmSetting(userId, settingDto);
+    return ResponseDto.res("SUCCESS");
+  }
 }
--- bangtong/back/src/main/java/com/jisang/bangtong/model/alarmSetting/AlarmSetting.java
+++ bangtong/back/src/main/java/com/jisang/bangtong/model/alarmSetting/AlarmSetting.java
@@ -1,37 +1,42 @@
 package com.jisang.bangtong.model.alarmSetting;
 
 import com.jisang.bangtong.dto.alarmsetting.AlarmSettingDto;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class AlarmSetting {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long alarmId;
 
-    @Column
-    private Long userId = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long alarmId;
 
-    @Column(nullable = false, columnDefinition = "boolean default false")
-    private boolean alarmPhoneChat;
+  //    TODO: userID
+  private Long userId = 1L;
 
-    @Column(nullable = false, columnDefinition = "boolean default false")
-    private boolean alarmPhoneComplete;
+  @Column(nullable = false, columnDefinition = "boolean default false")
+  private boolean alarmPhoneChat;
 
-    @Column(nullable = false, columnDefinition = "boolean default false")
-    private boolean alarmPhoneInterest;
+  @Column(nullable = false, columnDefinition = "boolean default false")
+  private boolean alarmPhoneComplete;
 
-    @Column(nullable = false, columnDefinition = "boolean default false")
-    private boolean alarmEmailChat;
+  @Column(nullable = false, columnDefinition = "boolean default false")
+  private boolean alarmPhoneInterest;
 
-    @Column(nullable = false, columnDefinition = "boolean default false")
-    private boolean alarmEmailComplete;
+  @Column(nullable = false, columnDefinition = "boolean default false")
+  private boolean alarmEmailChat;
 
-    @Column(nullable = false, columnDefinition = "boolean default false")
-    private boolean alarmEmailInterest;
+  @Column(nullable = false, columnDefinition = "boolean default false")
+  private boolean alarmEmailComplete;
 
-    public void updatealarmsetting(Long userId, AlarmSettingDto settingDto) {
-    }
+  @Column(nullable = false, columnDefinition = "boolean default false")
+  private boolean alarmEmailInterest;
+
+  public void updatealarmsetting(Long userId, AlarmSettingDto settingDto) {
+  }
 }
