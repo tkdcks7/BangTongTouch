fcb8e71f8d0d80d218006e1fe64d1cc2e6aaba06
강상찬 <tkdcks7@gmail.com>
2024-07-18 10:14:46 +0900

    feat: SignupPage axios 코드 작성, InputBox 코드 일부 수정

--- bangtong/front/src/components/page/SignupPage.tsx
+++ bangtong/front/src/components/page/SignupPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 // 컴포넌트 불러오기
 import TextBox from "../atoms/TextBox";
@@ -17,10 +18,29 @@ const SignupPage: React.FC = () => {
 
   const handleSignUp = (e: any):void => {
     e.preventDefault();
-    const formData: FormData = new FormData();
-    // formData.append()
-
-  }
+    const formData: FormData = new FormData();  // formData 인스턴스 생성
+    const birthYear: string = socialNumber.slice(0, 6)  // 생일은 주민번호 앞자리를 자름
+    const gender:number = Number(socialNumber[6]) % 2 // 성별은 주민번호 뒷자리 첫 자를 숫자로 변환 후 2로 나눈 나머지를 반환. 추후 프로필수정에서 수정
+    // formData에 전송할 값을 append
+    formData.append("name", name)
+    formData.append("phone", phone)
+    formData.append("password", password)
+    formData.append("birthYear", birthYear)
+    formData.append("gender", gender.toString())
+    console.log(`email = ${email} 입니다.`)
+    console.log(`formData = ${formData} 입니다.`)
+    axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:8080/user/register/',
+      headers: {
+        enctype: 'multipart/form-data', // 파일 형식 확인
+        // 나중에는 인증 정보도 집어넣어야함.
+      },
+      data: formData,
+    })
+    .then(response => console.log('성공적으로 전송됐습니다.', response.data)) // 확인용. refactoring 시 지울 것
+    .catch(error => console.log('전송 실패'))
+    }
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <div className="text-3xl font-bold m-6">
@@ -72,7 +92,9 @@ const SignupPage: React.FC = () => {
           type="email"
           width={400}
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => {
+            console.log(email)
+            setEmail(e.target.value)}}
         />
         <InputBox
           placeholder="비밀번호"
@@ -93,7 +115,7 @@ const SignupPage: React.FC = () => {
           onChange={(e) => setPasswordVerification(e.target.value)}
         />
         <div className="flex justify-center mt-20">
-          <Btn text="다음" backgroundColor="lime-500" textColor="white"/>
+          <Btn text="다음" backgroundColor="lime-500" textColor="white" onClick={handleSignUp}/>
         </div>
       </form>
     </div>
