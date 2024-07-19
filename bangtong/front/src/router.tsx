783c35d8a176e37c0f766baa85d7f98c6c7b8339
강상찬 <tkdcks7@gmail.com>
2024-07-19 14:42:01 +0900

    fix: router.tsx 중첩 라우팅 정리

--- bangtong/front/src/router.tsx
+++ bangtong/front/src/router.tsx
@@ -1,29 +1,32 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import LoginPage from './components/page/LoginPage';
-import SignupPage from './components/page/SignupPage';
-import FindSelectPage from './components/page/FindSelectPage';
-import IdFindPage from './components/page/IdFindPage';
-import PwFindPage from './components/page/PwFindPage';
-import MainPage from './components/page/MainPage';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import LoginPage from "./components/page/LoginPage";
+import SignupPage from "./components/page/SignupPage";
+import FindSelectPage from "./components/page/FindSelectPage";
+import IdFindPage from "./components/page/IdFindPage";
+import PwFindPage from "./components/page/PwFindPage";
+import MainPage from "./components/page/MainPage";
 
 // 로그인페이지와 회원가입페이지는 Nav가 없기 때문에 Layout 밖에 선언함
 const Router: React.FC = () => {
   return (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/user/login' element={<LoginPage />} />
-      <Route path='/user/register' element={<SignupPage />} />
-      <Route path='/user/FindSelectPage' element={<FindSelectPage />} />
-      <Route path='/user/IdFindPage' element={<IdFindPage />} />
-      <Route path='/user/PwFindPage' element={<PwFindPage />} />
-      <Route path='/' element={<Layout />} >
-        <Route path='' element={<MainPage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="user">
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<SignupPage />} />
+          <Route path="FindSelectPage" element={<FindSelectPage />} />
+          <Route path="IdFindPage" element={<IdFindPage />} />
+          <Route path="PwFindPage" element={<PwFindPage />} />
+        </Route>
+        <Route path="" element={<Layout />}>
+          <Route path="" element={<MainPage />} />
+        </Route>
+        {/* <Route path="*" element={<ErrorPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
