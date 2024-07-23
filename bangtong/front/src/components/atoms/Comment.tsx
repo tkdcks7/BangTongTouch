3a3e89213fa8a22c04d44f6d6f1f267d974a49db
강상찬 <tkdcks7@gmail.com>
2024-07-23 14:15:51 +0900

    feat: 댓글 기능 동적 요소 작성

--- bangtong/front/src/components/atoms/Comment.tsx
+++ bangtong/front/src/components/atoms/Comment.tsx
@@ -1,34 +1,65 @@
 import React from "react";
 
+import SubComment from "./SubComment";
 /**
  *    예시   <Comment comment_id="까치" content="그건 좀;;" date={273890147923} />
  *  comment_id 작성자명 content 내용 date 시간 long long 형 milisec 단위
  */
 
-interface ChildProps {
-  comment_id: string;
-  content: string;
-  date: number;
+interface SubCommnetProps {
+  commentId: number;
+  commentUser: any;
+  commentContent: string;
+  commentDate: string;
 }
 
-const Comment: React.FC<ChildProps> = ({ comment_id, content, date }) => {
+interface ChildProps {
+  commentId: number;
+  userName: string;
+  content: string;
+  date: string;
+  comments: Array<SubCommnetProps>;
+}
+
+const Comment: React.FC<ChildProps> = ({
+  commentId,
+  userName,
+  content,
+  date,
+  comments,
+}) => {
   return (
-    <div className="flex pl-1 flex-col py-0.5">
-      <div className="text-lg">{comment_id}</div>
-      <div className="text-base">{content}</div>
-      <div className="text-sm">{formatTimestamp(date)}</div>
-    </div>
+    <React.Fragment>
+      <div className="flex pl-1 py-0.5">
+        <div className="flex-initial text-sm w-12">{userName}</div>
+        <div className="flex-1 text-base break-words overflow-hidden whitespace-pre-wrap">
+          {content}
+        </div>
+        <div className="flex-initial text-xs w-16">{formatTimestamp(date)}</div>
+      </div>
+      {comments.length > 0 &&
+        comments.map((cmt) => {
+          console.log("cmt=", cmt);
+          return (
+            <SubComment
+              key={cmt.commentId}
+              commentId={cmt.commentId}
+              userName={cmt.commentUser.userName}
+              content={cmt.commentContent}
+              date={cmt.commentDate}
+            />
+          );
+        })}
+    </React.Fragment>
   );
 };
-function formatTimestamp(timestamp: number): string {
+function formatTimestamp(timestamp: string): string {
   const date = new Date(timestamp);
-
-  const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
 
-  return `${year}.${month}.${day}. ${hours}:${minutes}`;
+  return `${month}.${day}. ${hours}:${minutes}`;
 }
 export default Comment;
