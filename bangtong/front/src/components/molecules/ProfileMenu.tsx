fd4750775dcc766322d215b6f02fe3aae11ed6dc
Jun <dmg05135@gmail.com>
2024-07-24 16:38:58 +0900

    feat: 마이페이지 회원정보 수정 창, 알림 권한 설정 창 추가

--- bangtong/front/src/components/molecules/ProfileMenu.tsx
+++ bangtong/front/src/components/molecules/ProfileMenu.tsx
@@ -3,7 +3,6 @@ import React from "react";
 const ProfileMenu: React.FC = () => {
   return (
     <div>
-      <h2 className="w-full text-xl text-left mt-5 text-lime-500 font-bold">나의 거래</h2>
       <div id="accordion-collapse" data-accordion="collapse">
         <h2 id="accordion-collapse-heading-1">
           <button type="button" className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
