b13411e3c58f09e6b941821efae9a2d0fb04f847
Jun <dmg05135@gmail.com>
2024-07-29 09:45:40 +0900

    feat: 채팅 디테일 페이지 레이아웃 추가

--- bangtong/front/src/components/organism/ChatDetail.tsx
+++ bangtong/front/src/components/organism/ChatDetail.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+// 컴포넌트
+import RollBackBtn from "../atoms/RollBackBtn";
+import ChatMsgBox from "../atoms/ChatMsgBox";
+
+// 이미지 소스
+import defaultProfile from "../../assets/defaultprofile.jpg";
+import InputBox from "../molecules/InputBox";
+
 const ChatDetail: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <RollBackBtn />
+      <div className="flex mt-10">
+        <img
+          src={defaultProfile}
+          alt="프로필 사진"
+          className="w-10 h-10 rounded-full me-3"
+        />
+        <ChatMsgBox message="안녕하세요~" date="2024-07-26 17:41" />
+      </div>
+      <div className="mt-10">
+        <InputBox placeholder="채팅 입력" width={"auto"} buttonType="send" />
+      </div>
+      <button className="p-2 px-4 bg-yellow-400 rounded-full">+</button>
+    </div>
+  );
 };
 
 export default ChatDetail;
