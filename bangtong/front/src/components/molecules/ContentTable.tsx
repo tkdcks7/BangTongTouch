08d3ec2ca908d1c9c7822b2ee60b626e29eee1cd
Jun <dmg05135@gmail.com>
2024-07-30 17:46:17 +0900

    feat: 커뮤니티 게시글 목록 백단과 연동하여 데이터 출력

--- bangtong/front/src/components/molecules/ContentTable.tsx
+++ bangtong/front/src/components/molecules/ContentTable.tsx
@@ -1,12 +1,38 @@
 import React from "react";
 
 // 게시글 데이터
-import { contents } from "../../data.js";
+import { contents as contentsData } from "../../data.js";
 
 // 컴포넌트
 import Post from "../atoms/Post";
+import { Link } from "react-router-dom";
 
-const ContentTable: React.FC = () => {
+interface boardWriter {
+  userBirthYear: number;
+  userEmail: string;
+  userGender: number;
+  userId: number;
+  userIsAdmin: boolean;
+  userIsBanned: boolean;
+  userIsDeleted: boolean;
+  userNickname: string;
+  userPhone: string;
+  userProvider: string;
+  userRegisterDate: string;
+}
+
+interface Content {
+  boardId: number;
+  boardTitle: string;
+  boardWriter: boardWriter;
+  boardDate: string;
+}
+
+interface ContentTableProps {
+  contents: Array<Content>;
+}
+
+const ContentTable: React.FC<ContentTableProps> = ({ contents }) => {
   return (
     <table className="border-collapse w-full text-sm text-nowrap">
       <thead className="bg-yellow-100">
@@ -17,13 +43,13 @@ const ContentTable: React.FC = () => {
         </tr>
       </thead>
       <tbody>
-        {contents.map((item) => (
+        {contents.map((content) => (
           <Post
-            key={item.boardId}
-            id={item.boardId}
-            title={item.boardTitle}
-            writer={item.boardWriter}
-            tag={item.boardDate.slice(11, 16)}
+            key={content.boardId}
+            id={content.boardId}
+            title={content.boardTitle}
+            writer={content.boardWriter.userNickname}
+            date={content.boardDate}
           />
         ))}
       </tbody>
--- bangtong/front/src/components/organism/CommunityMain.tsx
+++ bangtong/front/src/components/organism/CommunityMain.tsx
@@ -1,17 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 // 컴포넌트
 import MultiBtn from "../molecules/MultiBtn";
 import ContentTable from "../molecules/ContentTable";
 import InputBox from "../molecules/InputBox";
 import IconBtn from "../atoms/IconBtn";
-import ImgBox from "../atoms/ImgBox";
+import { Pagination, ConfigProvider } from "antd";
 
 // 이미지 소스
 import Pencil from "../../assets/Pencil.png";
-import { Link } from "react-router-dom";
+
+interface BoardWriter {
+  userBirthYear: number;
+  userEmail: string;
+  userGender: number;
+  userId: number;
+  userIsAdmin: boolean;
+  userIsBanned: boolean;
+  userIsDeleted: boolean;
+  userNickname: string;
+  userPhone: string;
+  userProvider: string;
+  userRegisterDate: string;
+}
+
+interface Content {
+  boardId: number;
+  boardTitle: string;
+  boardWriter: BoardWriter;
+  boardDate: string;
+}
 
 const CommunityMain: React.FC = () => {
+  // 데이터 (받아온 게시글들)
+  const [page, setPage] = useState<number>(1);
+  const [contents, setContents] = useState<Content[]>([]);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  // pagination 글로벌 디자인 토큰
+  const theme = {
+    token: {
+      colorBgTextHover: "#E9FFE7",
+      colorPrimary: "#129B07",
+    },
+  };
+
+  const loadData = () => {
+    axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_BACKEND_URL}/boards/list`,
+      data: {
+        pageNo: `${page - 1}`,
+        size: "10",
+      },
+    })
+      .then((response) => {
+        setContents(response.data.content);
+        setIsLoaded(true);
+        console.log(response.data.content);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  useEffect(() => {
+    loadData();
+  }, [page]);
+
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="mt-5 w-full flex items-center justify-center">
@@ -30,12 +86,23 @@ const CommunityMain: React.FC = () => {
       <div className="my-5 md:w-full">
         <MultiBtn />
       </div>
-      <ContentTable />
+      {isLoaded && <ContentTable contents={contents} />}
       <div className="w-full p-2 mt-5 flex justify-end">
-        <Link className="bg-yellow-300 p-2 rounded-xl" to={"write"}>
+        <Link className="bg-yellow-300 p-3 rounded-xl shadow-lg" to={"write"}>
           <IconBtn imgSrc={Pencil} size={30} />
         </Link>
       </div>
+      <ConfigProvider theme={theme}>
+        <Pagination
+          current={page}
+          total={50}
+          className="mt-10"
+          responsive
+          onChange={(pageNumber) => {
+            setPage(pageNumber);
+          }}
+        />
+      </ConfigProvider>
     </div>
   );
 };
