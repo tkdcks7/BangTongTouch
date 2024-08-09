0ce77d3e3bf824f9ecd6609eac29363f123d5e78
민서령 <msr980929@gmail.com>
2024-08-09 17:45:52 +0900

    test: WebRTC remote video 연결 여부 테스트

--- bangtong/front/src/utils/SocketService.ts
+++ bangtong/front/src/utils/SocketService.ts
@@ -7,8 +7,7 @@ class SocketService {
   constructor() {
     this.client = new Client({
       webSocketFactory: () =>
-        new SockJS(`https://i11d206.p.ssafy.io/api/signaling`),
-      // new SockJS(`${process.env.REACT_APP_BACKEND_URL}/signaling`),
+        new SockJS(`${process.env.REACT_APP_BACKEND_URL}/signaling`),
       onConnect: () => {
         console.log("Socket Connected");
       },
