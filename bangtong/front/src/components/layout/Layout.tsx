ae58b6dfb0791bd34775b6235fa425f20e644d87
강상찬 <tkdcks7@gmail.com>
2024-08-09 12:58:53 +0900

    fix: mainpage router 문제 수정

--- bangtong/front/src/components/layout/Layout.tsx
+++ bangtong/front/src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 // 컴포넌트
 import MMenuBar from "../organism/MMenuBar"; // 모바일 메뉴바 (알림, 햄버거버튼)
@@ -6,27 +7,27 @@ import MNavBar from "../organism/MNavBar"; // 모바일 네비게이션바
 import PcNavBar from "../organism/PcNavBar"; // PC 네비게이션바
 import PcFooter from "../organism/PcFooter"; // PC 푸터
 
-import { Outlet } from "react-router-dom";
-
 const Layout: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="md:hidden">
-        <MMenuBar />
+    <>
+      <div className="flex flex-col min-h-screen">
+        <div className="md:hidden">
+          <MMenuBar />
+        </div>
+        <div className="hidden md:block">
+          <PcNavBar />
+        </div>
+        <div className="flex flex-1 flex-col items-center mx-10 mb-10">
+          <Outlet />
+        </div>
+        <div className="md:hidden">
+          <MNavBar />
+        </div>
+        <div className="hidden md:block">
+          <PcFooter />
+        </div>
       </div>
-      <div className="hidden md:block">
-        <PcNavBar />
-      </div>
-      <div className="flex flex-1 flex-col items-center mx-10 mb-10">
-        <Outlet />
-      </div>
-      <div className="md:hidden">
-        <MNavBar />
-      </div>
-      <div className="hidden md:block">
-        <PcFooter />
-      </div>
-    </div>
+    </>
   );
 };
 
