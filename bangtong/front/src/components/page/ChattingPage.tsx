1ab609930e8c50a1cd2a04d92e2280e9f243596e
Jun <dmg05135@gmail.com>
2024-08-13 17:54:28 +0900

    design: 채팅 페이지 반응형 웹, 다크모드 구현

--- bangtong/front/src/components/page/ChattingPage.tsx
+++ bangtong/front/src/components/page/ChattingPage.tsx
@@ -8,12 +8,14 @@ const ChattingPage: React.FC = () => {
   return (
     <div className="flex items-center justify-center">
       <div
-        className="h-full w-80 hidden md:block bg-gray-50 overflow-auto"
+        className="w-80 h-[700px] hidden md:block bg-lime-400 dark:bg-lime-800 overflow-auto text-black rounded-xl"
         id="product-list"
       >
         <ChatBox />
       </div>
-      <Outlet />
+      <div className="bg-lime-50 dark:bg-lime-600 text-black rounded-xl mb-[70px] md:mb-0">
+        <Outlet />
+      </div>
     </div>
   );
 };
