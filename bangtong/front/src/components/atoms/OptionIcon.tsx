81dd6dd3a6b7dc25c4ddf4c818d0c1a5e264643c
Jun <dmg05135@gmail.com>
2024-08-14 11:56:15 +0900

    feat:마이페이지 내가올린 매물 조회

--- bangtong/front/src/components/atoms/OptionIcon.tsx
+++ bangtong/front/src/components/atoms/OptionIcon.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 interface OptionProps {
-    src: string;
-    text: string;
+  src: string;
+  text: string;
 }
 
-const OptionIcon: React.FC<OptionProps> = ({src, text}) => {
-    return (
-        <div className={`m-2 flex flex-col items-center`}>
-            <img src={src} alt={text} width={50}/>
-            <p>{text}</p>
-        </div>
-    );
+const OptionIcon: React.FC<OptionProps> = ({ src, text }) => {
+  return (
+    <div className={`m-2 flex flex-col items-center`}>
+      <img src={src} alt={text} width={40} />
+      <p className="text-base font-bold">{text}</p>
+    </div>
+  );
 };
 
 export default OptionIcon;
--- bangtong/front/src/components/molecules/ProductOptions.tsx
+++ bangtong/front/src/components/molecules/ProductOptions.tsx
@@ -12,70 +12,68 @@ import GreenTv from "../../assets/GreenTv.png";
 import GreenMicrowave from "../../assets/GreenMicrowave.png";
 import GreenAirConditioner from "../../assets/GreenAirconditioner.png";
 
-
 interface ProductProps {
-    dark: boolean;
-    options: number;
-    isPc: boolean;
+  dark: boolean;
+  options: number;
+  isPc: boolean;
 }
 
-const ProductOptions: React.FC<ProductProps> = ({options, isPc, dark}) => {
-    const optionList = [
-        "텔레비전",
-        "침대",
-        "전자레인지",
-        "에어컨",
-        "세탁기",
-        "냉장고",
-        "가스레인지",
-    ];
+const ProductOptions: React.FC<ProductProps> = ({ options, isPc, dark }) => {
+  const optionList = [
+    "텔레비전",
+    "침대",
+    "전자레인지",
+    "에어컨",
+    "세탁기",
+    "냉장고",
+    "가스레인지",
+  ];
 
-    const iconList = [
-        GreenTv,
-        GreenBed,
-        GreenMicrowave,
-        GreenAirConditioner,
-        GreenWashingMachine,
-        GreenFridge,
-        GreenStove,
-    ];
+  const iconList = [
+    GreenTv,
+    GreenBed,
+    GreenMicrowave,
+    GreenAirConditioner,
+    GreenWashingMachine,
+    GreenFridge,
+    GreenStove,
+  ];
 
-    const numberArray: number[] = [0, 0, 0, 0, 0, 0, 0];
+  const numberArray: number[] = [0, 0, 0, 0, 0, 0, 0];
 
-    // 편의시설 비트마스킹하는 함수
-    const bitMaskDecode = (num: number) => {
-        let val = num;
-        numberArray.forEach((el, idx) => {
-            if (val >= 2 ** (7 - idx)) {
-                numberArray[idx] = 1;
-                val -= 2 ** (7 - idx);
-            }
-        });
-        return val;
-    };
-    bitMaskDecode(options);
+  // 편의시설 비트마스킹하는 함수
+  const bitMaskDecode = (num: number) => {
+    let val = num;
+    numberArray.forEach((el, idx) => {
+      if (val >= 2 ** (7 - idx)) {
+        numberArray[idx] = 1;
+        val -= 2 ** (7 - idx);
+      }
+    });
+    return val;
+  };
+  bitMaskDecode(options);
 
-    return (
-
-        <div>
-            <h1 className={isPc ? "hidden" : "text-2xl font-black"}>옵션</h1>
-            <div className="flex flex-wrap justify-start items-center mt-5">
-                {numberArray.map((el: any, idx: number) => {
-                    if (el) {
-                        return (
-                            <OptionIcon
-                                key={optionList[idx]}
-                                src={iconList[idx]}
-                                text={optionList[idx]}
-                            />
-                        );
-                    } else {
-                        return null;
-                    }
-                })}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className={isPc ? "hidden" : "text-2xl font-black"}>옵션</h1>
+      <div className="flex flex-wrap justify-start items-center mt-5">
+        {numberArray.map((el: any, idx: number) => {
+          if (el) {
+            return (
+              <OptionIcon
+                key={optionList[idx]}
+                src={iconList[idx]}
+                text={optionList[idx]}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ProductOptions;
