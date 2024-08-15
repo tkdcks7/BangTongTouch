c7063658b629e9aedca4b3b1835774ee701e14b1
jiwon <idjjm95@gmail.com>
2024-08-15 12:30:32 +0900

    fix :[BE]interest entity 수정

--- bangtong/back/src/main/java/com/jisang/bangtong/model/interest/Interest.java
+++ bangtong/back/src/main/java/com/jisang/bangtong/model/interest/Interest.java
@@ -1,6 +1,5 @@
 package com.jisang.bangtong.model.interest;
 
-import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.jisang.bangtong.model.product.Product;
 import com.jisang.bangtong.model.user.User;
 import jakarta.persistence.Entity;
@@ -10,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -29,7 +27,7 @@ public class Interest {
   @JoinColumn(name="user_id", foreignKey = @ForeignKey(name="fk_interest_user"), nullable = false)
   private User user;
 
-  @OneToOne
+  @ManyToOne
   @JoinColumn(name="product_id", foreignKey = @ForeignKey(name = "fk_interest_product"), nullable = false)
   private Product product;
 }
